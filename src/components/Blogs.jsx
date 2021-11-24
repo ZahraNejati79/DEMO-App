ecbdea6062ef447ddf9d7408b30deaf5acd5613e
ZahraNejati <nejatizahra4@gmail.com>
2021-11-24 07:44:11 +0330

    query-string & queryString.parse

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,7 +1,9 @@
 import { Link } from "react-router-dom";
-const Blogs = (props) => {
-  console.log(props);
-  const id = props.match.params.id || 1;
+import queryString from "query-string";
+const Blogs = ({ location, match }) => {
+  const id = match.params.id || 1;
+  const query = queryString.parse(location.search);
+  console.log(query);
   // console.log(props.match.params.id);
   return (
     <>
